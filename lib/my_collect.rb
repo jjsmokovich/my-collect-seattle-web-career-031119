d9a1e1bec1582f8d2d5d6b0aab3981a00104400a
Jesse Smokovich <jessesmokovich@yahoo.com>
2019-03-03 00:34:01 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(array)
    my_collection = []
   i = 0
   while i < array.length
-    my_collection << yield(array[i])
+    my_collection.push yield(array[i])
     i += 1
   end
   name_collection
